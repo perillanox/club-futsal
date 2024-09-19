93d8f7db17dd56c8a37bd51e551e473a7346e000
perillanox <proyectosxoan@gmail.com>
2024-09-19 10:42:36 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,8 +1,3 @@
-function toggleNav() {
-    const navbar = document.getElementById('navbar');
-    navbar.classList.toggle('active');
-}
-
 function showSection(sectionId) {
     const sections = document.querySelectorAll('main > section');
     sections.forEach(section => {
