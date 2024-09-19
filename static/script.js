0b0ecdb83736b75a867d41362189ee8413ba9c1f
perillanox <proyectosxoan@gmail.com>
2024-09-19 10:57:55 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -4,4 +4,18 @@ function showSection(sectionId) {
         section.classList.remove('active');
     });
     document.getElementById(sectionId).classList.add('active');
+    
+    // Reset subsections when navigating to a new section
+    const subsections = document.querySelectorAll('.subsection');
+    subsections.forEach(subsection => {
+        subsection.classList.remove('active');
+    });
+}
+
+function showSubsection(subsectionId) {
+    const subsections = document.querySelectorAll('.subsection');
+    subsections.forEach(subsection => {
+        subsection.classList.remove('active');
+    });
+    document.getElementById(subsectionId).classList.add('active');
 }
