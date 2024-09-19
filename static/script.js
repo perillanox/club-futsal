ee858f5012d4849021fed6f7234ef8a80f44580f
perillanox <proyectosxoan@gmail.com>
2024-09-19 12:22:20 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -11,16 +11,3 @@ function showSection(sectionId) {
         selectedSection.style.display = 'block';
     }
 }
-
-// Efecto de resaltar los enlaces del menú al pasar el cursor
-const menuItems = document.querySelectorAll('nav a');
-
-menuItems.forEach(item => {
-    item.addEventListener('mouseenter', () => {
-        item.style.color = '#80ff80'; // Cambia a verde claro
-    });
-
-    item.addEventListener('mouseleave', () => {
-        item.style.color = 'white'; // Vuelve al blanco
-    });
-});
