b00329cbca2e59040a99339e75b429c37426435e
perillanox <proyectosxoan@gmail.com>
2024-09-19 12:04:02 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,3 +1,17 @@
+function showSection(sectionId) {
+    // Oculta todas las secciones
+    const sections = document.querySelectorAll('main > section');
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
+
+    // Muestra la sección seleccionada
+    const selectedSection = document.getElementById(sectionId);
+    if (selectedSection) {
+        selectedSection.style.display = 'block';
+    }
+}
+
 // Efecto de resaltar los enlaces del menú al pasar el cursor
 const menuItems = document.querySelectorAll('nav a');
 
